00fbee5690149b80a07fd16adae49451ef0b4d78
Alex Chapman <alex@farfromthere.net>
2023-11-08 13:31:36 +1100

    Enable Svelte reactive preprocessor, for debugging

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
+import { reactivePreprocess } from 'svelte-reactive-preprocessor'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
-    preprocess: preprocess({
-        postcss: true
-    }),
+    preprocess: [
+        preprocess({ postcss: true }),
+        reactivePreprocess()
+    ],
 
     kit: {
         adapter: adapter({ }),
