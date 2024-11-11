353db79e88d754b0e400b2bbc3fd0f558bc50f71
Alex Chapman <alex@farfromthere.net>
2024-11-11 11:38:59 +1100

    Time slider improvements: left-to-right, logarithmic, not so wide.

--- src/TimeControls.svelte
+++ src/TimeControls.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import * as d3Scale from 'd3-scale';
+
     import { DateInput } from 'date-picker-svelte'
 
     export let minutesToShow: number
@@ -14,11 +16,15 @@
         minutesToShow = minutes
     }
 
+    const minutesRange = [10, 1440] // 10 minutes to 24 hours
+    const sliderRange = [0, 999]
+    const sliderScale = d3Scale.scaleLog(minutesRange, sliderRange)
 </script>
 
 <div class="w-full pb-0 mt-2">
+
     <DateInput bind:value={date} placeholder={"current"} format={"yyyy-MM-dd"} max={new Date(Date.now())} />
-    <input class="w-full" bind:value={minutesToShow} type="range" min="10" max="1440" id="minutesToShow" style="direction: rtl;"/>
+    <input class="w-96 max-w-full" value={sliderScale(minutesToShow)} on:input={e => minutesToShow = Math.round(sliderScale.invert(parseInt(e.currentTarget.value)))} type="range" min={sliderRange[0]} max={sliderRange[1]} id="minutesToShow" /><br />
     <button class="{minutesToShow == 10 ? 'enabled' : ''}" on:click={() => setCurrent(10)}>10 mins</button>
     <button class="{minutesToShow == 60 ? 'enabled' : ''}" on:click={() => setCurrent(60)}>1 hour</button>
     <button class="{minutesToShow == 4 * 60 ? 'enabled' : ''}" on:click={() => setCurrent(4 * 60)}>4 hours</button>
