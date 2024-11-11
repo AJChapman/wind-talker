18dfc51216f9b3456822e49f134939a856c94c14
Alex Chapman <alex@farfromthere.net>
2024-11-11 11:39:00 +1100

    Add site dirOnCentre

--- src/site.ts
+++ src/site.ts
@@ -5,7 +5,7 @@ export interface SiteGroup {
 
 export interface SiteDirection {
     centerDeg: number // The center of the direction band, in degrees
-    halfWidthDeg: number /// Half the width of the direction band, in degrees
+    halfWidthDeg: number // Half the width of the direction band, in degrees
 }
 
 export interface Site {
@@ -19,5 +19,11 @@ export interface Site {
     speedMaxMph: number
     directions: Array<SiteDirection>
     altitudeFt: number
+
+    // Adjustment for directions as they come in (TODO: check this)
     dirAdjust: number
+
+    // The direction at the center of the direction graph.
+    // If not defined then graph spans from 0 to 359 degrees.
+    dirOnCentre: number | undefined
 }
