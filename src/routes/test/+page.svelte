24fb06905a15e189b6c41a48603eb5bafa3e6e55
Alex Chapman <alex@farfromthere.net>
2024-11-11 11:38:59 +1100

    Fix test page, was pointed at Mt. Emu

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import WindTalkerPage from '$lib/WindTalkerPage.svelte'
-    import { mtemu as site } from '$lib/freeflightwx-sites'
+    import { test as site } from '$lib/freeflightwx-sites'
 </script>
 
 <WindTalkerPage {site} />
